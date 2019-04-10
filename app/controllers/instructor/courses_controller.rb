948bb8d3ee0c5c55edf3d63c517e11906f2204df
Michael Kozina <mike.kozina@gmail.com>
2019-04-10 20:09:27 +0000

    Listing Courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -11,13 +11,13 @@ class Instructor::CoursesController < ApplicationController
       redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
-    end  
+    end
   end
-  
+
   def show
     @course = Course.find(params[:id])
   end
-  
+
   private
 
   def course_params
