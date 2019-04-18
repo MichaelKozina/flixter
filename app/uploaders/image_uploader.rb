927bfba6e3c9af35d8e40ed42e2e8ea60e429010
Michael Kozina <mike.kozina@gmail.com>
2019-04-18 14:05:35 +0000

    Adjusting the Image Resolution

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,6 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Baseinclude CarrierWave::MiniMagick
+
+  include CarrierWave::MiniMagick
 
   if Rails.env.production?
     storage :fog
@@ -9,6 +11,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #storage :file
   #storage :aws
   #storage :fog
+  
+  process resize_to_fill: [800, 350]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
