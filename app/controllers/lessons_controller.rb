88ead257d071706de9ed0fc75cf093a1a7738716
Michael Kozina <mike.kozina@gmail.com>
2019-04-23 14:48:53 +0000

    Creating User Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :check_enrollment
+  
   def show
   end
   
@@ -7,5 +10,16 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+  
+  def check_enrollment 
+    unless current_user && (current_user.enrolled_in?(current_lesson.section.course) || current_user === current_lesson.section.course.user)
+      flash.alert = "You are not enrolled in this course"
+      redirect_to course_path(current_lesson.section.course)
+    end
+    
+    if current_user === current_lesson.section.course.user
+      flash.notice = "You are viewing your lesson as a student"
+    end
   end
 end
